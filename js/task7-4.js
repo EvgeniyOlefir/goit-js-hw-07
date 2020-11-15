e7cb5fed72ba4cf4d47cf00c62abf5cb331137ac
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2020-11-15 21:11:11 +0200

    Task 7-4 done

---
+++ js/task7-4.js
@@ -0,0 +1,39 @@
+// Задание 4
+// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
+
+// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+// <div id="counter">
+//   <button type="button" data-action="decrement">-1</button>
+//   <span id="value">0</span>
+//   <button type="button" data-action="increment">+1</button>
+// </div>
+
+// const decrementBtnEl = document.querySelector(
+//   '#counter > [data-action="decrement"]');
+// const incrementBtnEl = document.querySelector(
+//   '#counter > [data-action="increment"]');
+// const counterEl = document.querySelector('#counter > span');
+
+// через паттерн "объект ссылок"
+const refs = {
+  decrementBtnEl: document.querySelector('[data-action="decrement"]'),
+  incrementBtnEl: document.querySelector('[data-action="increment"]'),
+  counterValueEl: document.querySelector("#value"),
+};
+
+let counterValue = Number(refs.counterValueEl.textContent);
+refs.decrementBtnEl.addEventListener("click", onDecrementBtnClick);
+refs.incrementBtnEl.addEventListener("click", onIncrementBtnClick);
+
+function onDecrementBtnClick() {
+  counterValue === 0
+    ? (refs.counterValueEl.textContent = counterValue)
+    : (refs.counterValueEl.textContent = counterValue -= 1);
+}
+
+function onIncrementBtnClick() {
+  counterValue += 1;
+  refs.counterValueEl.textContent = counterValue;
+}
